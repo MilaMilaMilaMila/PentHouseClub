95dce00c2a066f54750fc1f8cdf2a2a50ea06d59
Milana Vorobeva <vorobey2317@mail.ru>
2023-10-06 22:30:43 +0300

    fix client and storage-service

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
 const baseURL = "http://localhost:8080"
 
-type IClient interface {
+type Client interface {
 	get(key string) (string, error)
 	set(key string, value string) error
 }
 
-type Client struct {
+type ClientImpl struct {
 }
 
 func main() {
 	var client Client
+	client = ClientImpl{}
+	if len(os.Args) == 1 {
+		os.Exit(3)
+	}
 	args := os.Args[1:]
 	if args[0] == "get" {
 		if len(args) < 2 {
@@ -32,10 +37,11 @@ func main() {
 		fmt.Println(resp)
 	} else if args[0] == "set" {
 		if len(args) < 3 {
-			fmt.Println("Invalid arguments. Key and value are required")
+			fmt.Println("Invalid arguments. Key and RespJson are required")
 			os.Exit(1)
 		}
 		err := client.set(args[1], args[2])
+		fmt.Println("here")
 		if err != nil {
 			fmt.Println(err.Error())
 		}
@@ -45,33 +51,68 @@ func main() {
 	}
 }
 
-func (client Client) get(key string) (string, error) {
-	url := fmt.Sprintf("%s/keys/$%s", baseURL, key)
-	resp, err := http.Get(url)
+func (client ClientImpl) get(key string) (string, error) {
+	url := fmt.Sprintf("%s/keys/get", baseURL)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return "", err
+		log.Print(err)
+		os.Exit(1)
 	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
-}
-
-func (client Client) set(key, value string) error {
-	url := fmt.Sprintf("%s/keys/$%s?value=$%s", baseURL, key, value)
-	req, err := http.NewRequest(http.MethodPut, url, nil)
-	if err != nil {
-		return err
-	}
+	q := req.URL.Query()
+	q.Add("key", key)
+	req.URL.RawQuery = q.Encode()
+	fmt.Println(req.URL.String())
 
 	clientR := &http.Client{}
-	_, err = clientR.Do(req)
-	if err != nil {
-		return err
+	resp, err1 := clientR.Do(req)
+	if err1 != nil {
+		panic(err1)
 	}
+
+	type RespJson struct {
+		Value   string `json:"value"`
+		Message string `json:"message"`
+		Error   string `json:"error"`
+	}
+	var respJson RespJson
+	if getResponseErr := json.NewDecoder(resp.Body).Decode(&respJson); getResponseErr != nil {
+		log.Fatalf("Get response json error. Err: %s", getResponseErr)
+	}
+
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			log.Fatalf("Close response body error. Err: %s", err)
+		}
+	}()
+
+	return respJson.Value, nil
+}
+
+func (client ClientImpl) set(key, value string) error {
+	url := fmt.Sprintf("%s/keys/set", baseURL)
+	req, err := http.NewRequest(http.MethodPut, url, nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	q := req.URL.Query()
+	q.Add("key", key)
+	q.Add("value", value)
+	req.URL.RawQuery = q.Encode()
+	fmt.Println(req.URL.String())
+
+	clientR := &http.Client{}
+	resp, doRequestErr := clientR.Do(req)
+	if doRequestErr != nil {
+		return doRequestErr
+	}
+
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			log.Fatalf("Close response body error. Err: %s", err)
+		}
+	}()
 
 	return nil
 }
--- cmd/storage-service/storage-service.go
+++ cmd/storage-service/storage-service.go
@@ -2,49 +2,52 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 )
 
-type IStorageService interface {
-	get(key string) (string, error)
-	set(key string, value string) error
+const serverPort = "8080"
+
+type Storage interface {
+	Get(key string) (string, error)
+	Set(key string, value string) error
 }
 
-type StorageService struct {
+type StorageImpl struct {
 }
 
-func (storageService StorageService) set(key string, value string) error {
+func (storage StorageImpl) Set(key string, value string) error {
 	file, err := os.OpenFile("cmd/storage-service/data.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
 		return err
 	}
 	defer func() {
 		if err = file.Close(); err != nil {
-			log.Fatal(err)
+			log.Fatalf("Close file error. Err: %s", err)
 		}
 	}()
 
-	line := "key=" + key + " " + "value=" + value
+	line := "key=" + key + " " + "value=" + value + "\n"
 	_, writeError := file.WriteString(line)
 	if writeError != nil {
-		log.Fatal(writeError)
+		log.Fatalf("Write data in file error. Err: %s", writeError)
 		return writeError
 	}
 	return nil
 }
 
-func (storageService StorageService) get(key string) (string, error) {
+func (storage StorageImpl) Get(key string) (string, error) {
 	file, err := os.OpenFile("cmd/storage-service/data.txt", os.O_RDONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	defer func() {
 		if err = file.Close(); err != nil {
-			log.Fatal(err)
+			log.Fatalf("Close file error. Err: %s", err)
 		}
 	}()
 
@@ -63,36 +66,100 @@ func (storageService StorageService) get(key string) (string, error) {
 			value = storageValue
 			flag = true
 		}
-
-		//fmt.Println(storageKey)
-		//fmt.Println(storageValue)
 	}
 
 	if !flag {
-		// здесь надо ошибку настроить типо нет совпадающего ключа
-		// может надо кастомную ошибку, я не знаю
 		keyError = os.ErrNotExist
+		log.Fatalf("Not existing key error. Err: %s", keyError)
 	} else {
 		keyError = nil
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	if scannerErr := scanner.Err(); err != nil {
+		log.Fatalf("Scanner error. Err: %s", scannerErr)
+		return "", scannerErr
 	}
 
 	return value, keyError
 }
 
+type StorageService interface {
+	Get(w http.ResponseWriter, r *http.Request)
+	Set(w http.ResponseWriter, r *http.Request)
+}
+
+type StorageServiceImpl struct {
+	Storage Storage
+}
+
+func (storageService StorageServiceImpl) Get(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query().Get("key")
+	value, getFunctionErr := storageService.Storage.Get(key)
+
+	respMessage := "OK"
+	respError := ""
+
+	if getFunctionErr != nil {
+		respMessage = "FAILED"
+		respError = fmt.Sprintf("Get function error. Err: %s", getFunctionErr)
+		log.Fatalf("Get function error. Err: %s", getFunctionErr)
+	}
+
+	resp := make(map[string]string)
+	resp["value"] = value
+	resp["message"] = respMessage
+	resp["error"] = respError
+	jsonResp, parseJsonErr := json.Marshal(resp)
+	if parseJsonErr != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", parseJsonErr)
+	}
+
+	if _, writeResponseErr := w.Write(jsonResp); writeResponseErr != nil {
+		log.Fatalf("Write response error. Err: %s", writeResponseErr)
+	}
+
+	return
+}
+
+func (storageService StorageServiceImpl) Set(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query().Get("key")
+	value := r.URL.Query().Get("value")
+
+	respMessage := "OK"
+	respError := ""
+
+	setFunctionErr := storageService.Storage.Set(key, value)
+	if setFunctionErr != nil {
+		respMessage = "FAILED"
+		respError = fmt.Sprintf("Set function error. Err: %s", setFunctionErr)
+		log.Fatalf("Set function error. Err: %s", setFunctionErr)
+	}
+
+	resp := make(map[string]string)
+	resp["status"] = respMessage
+	resp["error"] = respError
+	jsonResp, parseJsonErr := json.Marshal(resp)
+	if parseJsonErr != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", parseJsonErr)
+	}
+
+	if _, writeResponseErr := w.Write(jsonResp); writeResponseErr != nil {
+		log.Fatalf("Write response error. Err: %s", writeResponseErr)
+	}
+
+	return
+}
+
 func main() {
-	var storageService IStorageService
-	storageService = StorageService{}
-	err1 := storageService.set("milasha", "Vorobusha")
-	if err1 != nil {
-		log.Fatal(err1)
-	}
-	value, err2 := storageService.get("milasha")
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-	fmt.Println(value)
+	var storageService StorageService
+	storageService = StorageServiceImpl{Storage: StorageImpl{}}
+
+	setUrl := fmt.Sprintf("/keys/set")
+	getUrl := fmt.Sprintf("/keys/get")
+
+	http.HandleFunc(getUrl, storageService.Get)
+	http.HandleFunc(setUrl, storageService.Set)
+	addr := fmt.Sprintf(":%s", serverPort)
+	setListenPortError := http.ListenAndServe(addr, nil)
+	log.Fatalf("Listen and serve port failed. Err: %s", setListenPortError)
 }
